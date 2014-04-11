fad982da8d5265a2e4fa262c7b850f2804395edc
elkorn <helluinster@gmail.com>
2014-04-11 16:40:22 +0200

    [qe] Adding initial delete button styling.

--- application/todo-list/config/application.rb
+++ application/todo-list/config/application.rb
@@ -20,5 +20,6 @@ module TodoList
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
   end
 end
