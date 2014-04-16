a65ec056576476f9368222123044e097297dd395
Wojciech Gawronski <afronski@gmail.com>
2014-04-17 00:19:28 +0200

    Task BDD_3

--- application/todo-list/features/step_definitions/lists.rb
+++ application/todo-list/features/step_definitions/lists.rb
@@ -1,25 +1,15 @@
-Given(/^there is one list named "([^"]+)" with (\d+) task for "([^"]+)"$/) do |listName, tasksNumber, username|
-  user = User.where(:username => username).first
-  list = List.create(:name => listName)
-
-  (0...tasksNumber.to_i).each do
-    list.tasks.build(:title => random_task_name())
-  end
-
-  list.user = user
-  list.save!
-end
-
-Given(/^there is one list named "([^"]+)" with one item named "([^"]+)"$/) do |listName, itemName|
-  list = List.create(:name => listName)
-  list.tasks.build(:title => itemName)
-  list.save!
-end
-
-Given(/^there are lists titled "([^"]+)", "([^"]+)"$/) do |firstList, secondList|
-  List.create!(:name => firstList)
-  List.create!(:name => secondList)
-end
+# Zadanie BDD_3
+#
+# "Lista jest zamknięta kiedy wszystkie jej elementy są zrobione."
+#
+# Ostatnie zadanie z serii jest odrobinę trudniejsze, należy zaimplementować
+# przygotowane kroki zgodnie z opisem zamieszczonym w przechwytywanej frazie.
+#
+# Pomoc znajdziesz w aplikacji w plikach:
+#   - application/todo-list/app/models/list.rb
+#   - application/todo-list/app/models/task.rb
+#
+# Nie przejmuj się jeśli Ci nie pójdzie! Zawsze możesz wrócić lub przejść do następnego zadania :)
 
 Given(/^there is one list named "([^"]+)" with (\d+) random tasks?$/) do |listName, tasksNumber|
   list = List.create(:name => listName)
@@ -31,64 +21,12 @@ Given(/^there is one list named "([^"]+)" with (\d+) random tasks?$/) do |listNa
   list.save!
 end
 
-Given(/^there is one empty list with name "([^"]+)"$/) do |listName|
-  List.create!(:name => listName)
-end
-
-Given(/^"([^"]+)" should see "([^"]+)"$/) do |username, listName|
-  User.where(:username => username).first.lists.where(:name => listName).count.should == 1
-end
-
-Given(/^there are no lists$/) do
-  List.delete_all
-end
+# TODO: Brakujący krok!
 
 When(/^first visible task in list "([^"]+)" will be closed$/) do |listName|
-  list = List.where(:name => listName).first
-
-  list.tasks.first.close!
-  list.tasks.first.save!
-end
-
-When(/^"([^"]+)" will be opened$/) do |listName|
-  list = List.where(:name => listName).first
-
-  list.make_available!
-  list.save
-end
-
-When(/^"([^"]+)" will be created$/) do |listName|
-  List.create!(:name => listName)
-end
-
-When(/^new task with name "([^"]+)" is added to the "([^"]+)"$/) do |taskName, listName|
-  list = List.where(:name => listName).first
-
-  task = Task.create(:title => taskName)
-  task.list = list
-  task.save!
-end
-
-Then(/^"([^"]+)" has no tasks inside$/) do |listName|
-  List.where(:name => listName).first.tasks.count.should == 0
-end
-
-Then(/^"([^"]+)" should be available$/) do |listName|
-  List.where(:name => listName).first.available.should == true
+  pending
 end
 
 Then(/^"([^"]+)" should not be available$/) do |listName|
-  List.where(:name => listName).first.available.should == false
-end
-
-Then(/^"([^"]+)" should has today's date$/) do |listName|
-  List.where(:name => listName).first.date.should == Date.today
-end
-
-Then(/^"([^"]+)" has (\d+) tasks? inside$/) do |listName, tasksNumber|
-  List.where(:name => listName).first.tasks.count.should == tasksNumber.to_i
-end
-
-Then(/^"([^"]+)" should be in repository$/) do |listName|
-  List.where(:name => listName).count == 1
+  pending
 end
