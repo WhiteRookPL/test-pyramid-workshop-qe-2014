5b27816a402914f98852143a160d7d9b46cbb2be
Wojciech Gawronski <afronski@gmail.com>
2014-04-17 00:32:53 +0200

    Task UT_3

--- application/todo-list/spec/models/list_spec.rb
+++ application/todo-list/spec/models/list_spec.rb
@@ -2,34 +2,21 @@ require 'spec_helper'
 require 'custom_list_helpers'
 
 describe List do
-  it { should have_many :tasks }
-end
-
-describe List do
-  it { should belong_to :user }
-end
-
-describe List do
   before(:each) do
     @listName = "Test name"
     createList(@listName)
   end
 
-  it "should be available when none of subsequent tasks are closed" do
-    assertListAvailability(@listName, true)
-  end
+  # Zadanie UT_3.
+  #
+  # "Lista jest zamknięta kiedy wszystkie jej elementy są zrobione."
+  #
+  # Zaimplementuj treść testu jednostkowego aby wypełnić wymaganie opisane
+  # poniżej. W wykonaniu zadania pomoże Ci plik: ../custom_list_helpers.rb
+  #
+  # Przeanalizuj jego treść aby dobrze wykonać zadanie.
 
   it "should not be available when all of subsequent tasks are closed" do
-    closeAllTasks(@listName)
-    assertListAvailability(@listName, false)
-  end
-
-  it "should be available even when part of subsequent tasks are closed" do
-    closeNotAllTasks(@listName)
-    assertListAvailability(@listName, true)
-  end
-
-  after(:each) do
-    @listName = nil
+    # TODO
   end
 end
