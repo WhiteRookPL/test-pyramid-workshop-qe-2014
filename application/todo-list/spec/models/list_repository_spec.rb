7dd69a7bb5903b1468012e152016230f526bbbbe
Wojciech Gawronski <afronski@gmail.com>
2014-04-17 00:29:16 +0200

    After UT_2

--- application/todo-list/spec/models/list_repository_spec.rb
+++ application/todo-list/spec/models/list_repository_spec.rb
@@ -4,18 +4,20 @@ describe ListRepository do
   before(:each) do
     @repository = ListRepository.new
 
-    # TODO: Wypełnij poprawnie repozytorium, aby test poprawnie przechodził.
-  end
+    opened = List.create(:name => "Opened List")
+    closed = List.create(:name => "Closed List")
+    closed.close_list!
 
-  # Zadanie UT_2.
-  #
-  # "Lista zamknięta jest niedostępna dla użytkownika."
-  #
-  # Wypełnij poprawnie inicjalizację testów, tak aby spełnić
-  # wymagania poniższego testu.
+    @repository.add(opened)
+    @repository.add(closed)
+  end
 
   it "not available lists should not be retrieved from repository" do
     expect(@repository.all.count).to eq(1)
     expect(@repository.all.first.name).to eq("Opened List")
+  end
+
+  after(:each) do
+    @repository = nil
   end
 end
