4dc8a1461d66e602242b09e6b81f057c9cec87e8
Wojciech Gawronski <afronski@gmail.com>
2014-04-17 10:50:41 +0200

    Fixes for BAT files.

--- application/todo-list/bootstrap.bat
+++ application/todo-list/bootstrap.bat
@@ -1,2 +1,1 @@
-bundle install
-rake db:migrate & rake db:seed & rake db:test:clone & rake bower:install
+bundle install & rake db:migrate & rake db:seed & rake db:test:clone & rake bower:install
--- gui-tests/run_gui_tests.bat
+++ gui-tests/run_gui_tests.bat
@@ -1,5 +1,2 @@
-pushd ..\application\todo-list
-rake db:drop & rake db:migrate & rake db:seed & rake db:test:clone
-popd
-
+pushd ..\application\todo-list & rake db:drop & rake db:migrate & rake db:seed & rake db:test:clone & popd
 cucumber features -t ~@skip
