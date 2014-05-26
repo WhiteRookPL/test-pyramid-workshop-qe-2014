60e643f1ee5ed6f9b87f71d4dd97abb50e141b25
Korneliusz Caputa <helluinster@gmail.com>
2014-05-26 22:16:39 +0200

    [qe2014] Fixing the checkbox styles on Firefox. Closes #19.

--- application/todo-list/app/assets/javascripts/list.js
+++ application/todo-list/app/assets/javascripts/list.js
@@ -1,18 +1,43 @@
-(function ($) {
+(function($) {
     "use strict";
 
-    $(".task-list__task__delete").on("click", function (event) {
+    Function.prototype.debounce = function(threshold, execAsap) {
+
+        var func = this,
+            timeout;
+
+        return function debounced() {
+            var obj = this,
+                args = arguments;
+
+            function delayed() {
+                if (!execAsap)
+                    func.apply(obj, args);
+                timeout = null;
+            }
+
+            if (timeout)
+                clearTimeout(timeout);
+            else if (execAsap)
+                func.apply(obj, args);
+
+            timeout = setTimeout(delayed, threshold || 100);
+        };
+
+    };
+
+    $(".task-list__task__delete").on("click", function(event) {
         var $this = $(this);
 
-        $this.prop("disabled", true);
+        $this.attr("disabled", true);
 
         $.ajax({
             url: "/lists/" + $this.attr("data-list-id") + "/tasks/" + $this.attr("data-task-id"),
             type: "DELETE",
-            success: function () {
+            success: function() {
                 window.location.reload();
             },
-            error: function (error) {
+            error: function(error) {
                 console.error(error);
             }
         });
@@ -21,38 +46,46 @@
         return false;
     });
 
-    $(".task-state").on("mousedown", function () {
+    $(".task-state + label").on("mousedown", function(event) {
         var $this = $(this);
-        $this.one("mouseup", function (event) {
-            var originalChecked = $this.prop("checked");
-            var taskContainer = $this.parents(".task-list__task");
-            $this.prop("disabled", true);
+        var $cb = $this.prev();
+        var originalChecked = $cb.prop("checked");
+        if ($cb.attr("disabled")) {
+            return false;
+        }
+
+        $(document).one("mouseup", function(event) {
+            var taskContainer = $cb.parents(".task-list__task");
+            $cb.attr("disabled", "disabled");
 
             $.ajax({
-                url: "/lists/" + $this.attr("data-list-id") + "/tasks/" + $this.attr("data-task-id") +
+                url: "/lists/" + $cb.attr("data-list-id") + "/tasks/" + $cb.attr("data-task-id") +
                     "/toggle",
                 type: "POST",
-                success: function () {
-                    $this.prop("disabled", false);
+                success: function() {
+                    $cb.prop("checked", !originalChecked);
+                    $this.text((originalChecked ? "Todo" : "Done"));
                     taskContainer.toggleClass("task-list__task--done");
-                    $this.prop("checked", !originalChecked);
+                    $cb.removeAttr("disabled");
                 },
-                error: function (error) {
+                error: function(error) {
                     document.write(error.responseText);
                 }
             });
 
             event.preventDefault();
         });
+
+        event.stopImmediatePropagation();
     });
 
-    $("#new-task-title").on("keyup", function (event) {
+    $("#new-task-title").on("keyup", function(event) {
         if (event.keyCode === 13) {
             $("#add-new-task").click();
         }
     });
 
-    $("#add-new-task").on("click", function (event) {
+    $("#add-new-task").on("click", function(event) {
         var $name = $("#new-task-title");
         var name = sanitize($name.val().trim());
         var $this = $(this);
@@ -60,19 +93,19 @@
         $name.removeClass("invalid");
 
         if (name) {
-            $this.prop("disabled", true);
+            $this.attr("disabled", true);
 
             $.ajax({
                 url: "/lists/" + $this.attr("data-list-id") + "/tasks/new/" + name,
                 type: "PUT",
-                success: function () {
+                success: function() {
                     window.location.reload();
                 },
-                error: function (error) {
+                error: function(error) {
                     if (error.status === 409) {
                         window.TODO.notify("A task with such name already exists.");
                         $name.addClass("invalid");
-                        $this.prop("disabled", false);
+                        $this.attr("disabled", false);
                     }
                 }
             });
@@ -84,15 +117,15 @@
         return false;
     });
 
-    $("#reopen-list").on("click", function (event) {
+    $("#reopen-list").on("click", function(event) {
         var $this = $(this);
 
-        $this.prop("disabled", true);
+        $this.attr("disabled", true);
 
         $.ajax({
             url: "/lists/" + $this.attr("data-list-id") + "/reopen",
             type: "POST",
-            success: function () {
+            success: function() {
                 window.location.reload();
             },
             error: function(error) {
